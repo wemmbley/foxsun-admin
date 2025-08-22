36a58057d5300f49b1a2cea4d04d9b588a76728e
Holiev Rustam <golevv.rustam@gmail.com>
2025-08-22 11:27:31 +0000

    added dashbord wigets dummyes, added edit-page seo widget dummy

--- resources/views/pages/actions/edit.blade.php
+++ resources/views/pages/actions/edit.blade.php
@@ -9,23 +9,27 @@
                         <div class="card-header">
                             <ul class="nav nav-tabs card-header-tabs nav-fill" data-bs-toggle="tabs" role="tablist">
                                 <li class="nav-item" role="presentation">
-                                    <a href="#tab-home" class="nav-link active" data-bs-toggle="tab" aria-selected="false" role="tab" tabindex="-1"><!-- Download SVG icon from http://tabler.io/icons/icon/home --><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon me-2 icon-2"><path d="M5 12l-2 0l9 -9l9 9l-2 0"></path><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"></path><path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6"></path></svg>
-                                        Редакция
+                                    <a href="#tab-home" class="nav-link active" data-bs-toggle="tab" aria-selected="false" role="tab" tabindex="-1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 20 20"><g fill="#d1d5db"><path d="M15 6V4h-3v2H8V4H5v2H4c-.6 0-1 .4-1 1v8h14V7c0-.6-.4-1-1-1h-1z"/></svg>
+                                        <span class="ms-1">Редакция</span>
                                     </a>
                                 </li>
                                 <li class="nav-item" role="presentation">
-                                    <a href="#tab-seo" class="nav-link" data-bs-toggle="tab" aria-selected="false" role="tab" tabindex="-1"><!-- Download SVG icon from http://tabler.io/icons/icon/user --><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon me-2 icon-2"><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"></path><path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path></svg>
-                                        SEO
+                                    <a href="#tab-seo" class="nav-link" data-bs-toggle="tab" aria-selected="false" role="tab" tabindex="-1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 20 20"><g fill="#d1d5db"><path fill="none" d="M0 0h20v20H0z"/><path d="M18.15 5.94c.46 1.62.38 3.22-.02 4.48-.42 1.28-1.26 2.18-2.3 2.48-.16.06-.26.06-.4.06-.06.02-.12.02-.18.02-.06.02-.14.02-.22.02h-6.8l2.22 5.5c.02.14-.06.26-.14.34-.08.1-.24.16-.34.16H6.95c-.1 0-.26-.06-.34-.16-.08-.08-.16-.2-.14-.34l-1-5.5H4.25l-.02-.02c-.5.06-1.08-.18-1.54-.62s-.88-1.08-1.06-1.88c-.24-.8-.2-1.56-.02-2.2.18-.62.58-1.08 1.06-1.3l.02-.02 9-5.4c.1-.06.18-.1.24-.16.06-.04.14-.08.24-.12.16-.08.28-.12.5-.18 1.04-.3 2.24.1 3.22.98s1.84 2.24 2.26 3.86zm-2.58 5.98h-.02c.4-.1.74-.34 1.04-.7.58-.7.86-1.76.86-3.04 0-.64-.1-1.3-.28-1.98-.34-1.36-1.02-2.5-1.78-3.24s-1.68-1.1-2.46-.88c-.82.22-1.4.96-1.7 2-.32 1.04-.28 2.36.06 3.72.38 1.36 1 2.5 1.8 3.24.78.74 1.62 1.1 2.48.88zm-2.54-7.08c.22-.04.42-.02.62.04.38.16.76.48 1.02 1s.42 1.2.42 1.78c0 .3-.04.56-.12.8-.18.48-.44.84-.86.94-.34.1-.8-.06-1.14-.4s-.64-.86-.78-1.5c-.18-.62-.12-1.24.02-1.72s.48-.84.82-.94z"/></svg>
+                                        <span class="ms-1">SEO</span>
                                     </a>
                                 </li>
                                 <li class="nav-item" role="presentation">
-                                    <a href="#tab-analytics" class="nav-link" data-bs-toggle="tab" aria-selected="false" role="tab" tabindex="-1"><!-- Download SVG icon from http://tabler.io/icons/icon/user --><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon me-2 icon-2"><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"></path><path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path></svg>
-                                        Аналитика
+                                    <a href="#tab-analytics" class="nav-link" data-bs-toggle="tab" aria-selected="false" role="tab" tabindex="-1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 20 20"><g fill="#d1d5db"><path fill="none" d="M0 0h20v20H0z"/><path d="M18 18V2h-4v16h4zm-6 0V7H8v11h4zm-6 0v-8H2v8h4z"/></svg>
+                                        <span class="ms-1">Аналитика</span>
                                     </a>
                                 </li>
                                 <li class="nav-item" role="presentation">
-                                    <a href="#tab-cache" class="nav-link" data-bs-toggle="tab" aria-selected="true" role="tab"><!-- Download SVG icon from http://tabler.io/icons/icon/activity --><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon me-2 icon-2"><path d="M3 12h4l3 8l4 -16l3 8h4"></path></svg>
-                                        Кэширование
+                                    <a href="#tab-cache" class="nav-link" data-bs-toggle="tab" aria-selected="true" role="tab">
+                                        <svg xmlns="http://www.w3.org/2000/svg"  width="24" height="24" viewBox="0 0 20 20"><g fill="#d1d5db"><path fill="none" d="M0 0h20v20H0z"/><path d="M15.5 16H15c-.1-2.5-.6-4.4-3.3-6 2.6-1.6 3.2-3.5 3.3-6h.5c.6 0 1-.4 1-1s-.4-1-1-1h-11c-.6 0-1 .4-1 1s.4 1 1 1H5c.1 2.5.6 4.4 3.3 6-2.6 1.6-3.2 3.5-3.3 6h-.5c-.6 0-1 .4-1 1s.4 1 1 1h11c.6 0 1-.4 1-1s-.4-1-1-1z"/></svg>
+                                        <span class="ms-1">Кэширование</span>
                                     </a>
                                 </li>
                             </ul>
@@ -66,6 +70,41 @@
                                                 </p>
                                             </div>
                                         </div>
+                                        <hr>
+                                        <h3 class="mt-5 mb-2">Группа самого важного.</h3>
+                                        <div class="mt-3">
+                                            <label class="form-label">Title заголовок <span class="form-label-description">0/60</span></label>
+                                            <p class="text-muted m-0">Начать лучше релевантно, бренд сдвинуть в конец.</p>
+                                            <p class="text-muted m-0 mb-2">Желательно уложиться в 50-60 символов.</p>
+                                            <input type="text" class="form-control" placeholder="Title заголовок">
+                                        </div>
+                                        <div class="mt-3">
+                                            <label class="form-label">H1 заголовок <span class="text-muted ms-2">Отключено!</span> <span class="form-label-description">0/70</span></label>
+                                            <p class="text-muted m-0">По умолчанию берется из вкладки "Редакция".</p>
+                                            <p class="text-muted mb-2">Желательно уложиться в 70-80 символов.</p>
+                                            <input type="text" class="form-control" placeholder="H1 заголовок" disabled>
+                                        </div>
+                                        <div class="mt-3">
+                                            <label class="form-label">H1 описание</label>
+                                            <p class="text-muted m-0">Здесь делаем описанием страницы длинным, но с фокусом на ключевую фразу. Чем <b>чаще</b> она будет встречаться, тем <b>лучше</b>.</p>
+                                            <p class="text-muted mb-2">Подсказка: ключевая фраза всегда находится в title. Просто в точности расставляем ее здесь несколько раз и ваш SEO будет идеален.</p>
+                                            <div class="mb-3">
+                                                <textarea class="form-control" name="example-textarea-input" rows="6" placeholder="Описание страницы"></textarea>
+                                            </div>
+                                        </div>
+                                        <hr>
+                                        <h3 class="mt-5 mb-2">Группа менее важного.</h3>
+                                        <div class="mt-3">
+                                            <label class="form-label">Meta описание <span class="form-label-description">0/170</span></label>
+                                            <p class="text-muted mb-2">Желательно уложиться в 160-170 символов.</p>
+                                            <input type="text" class="form-control" placeholder="Meta заголовок">
+                                        </div>
+                                        <div class="mt-3">
+                                            <label class="form-label">Meta ключевые слова</label>
+                                            <p class="text-muted m-0">Просто перечисляем ключевые слова страницы.</p>
+                                            <p class="text-muted mb-2">Google сейчас это делает самостоятельно по блок "группа самого важного", так что это мусорный тег.</p>
+                                            <input type="text" class="form-control" placeholder="Meta ключевые слова">
+                                        </div>
                                     </div>
                                 </div>
                                 <div class="tab-pane" id="tab-analytics" role="tabpanel">
@@ -82,6 +121,9 @@
                                                 </p>
                                             </div>
                                         </div>
+
+                                        <!-- ACTIVE USERS STATISTIC: DESKTOP, MOBILE, TABLET, TV BY DAY-->
+                                        <!-- REFERALS SOURCES: GOOGLE, FB, INSTA, SOCIAL BY DAY-->
                                     </div>
                                 </div>
                                 <div class="tab-pane" id="tab-cache" role="tabpanel">
@@ -179,10 +221,68 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="subheader">SEO Оценка</div>
+                                    <div id="chart-active-users-3" class="position-relative" style="min-height: 130px;">
+                                        <div id="seo-chart" class="d-flex justify-content-center apexcharts-canvas apexcharts-theme-" style="width: 268px; height: 130px;"></div>
+                                    </div>
+                                    <div class="fs-3 fw-bold text-center mt-4">78/100</div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+    <script>
+        const gapPercent = 0.32;                 // 32% пустого низа
+        const gapDeg = 360 * gapPercent;         // ~79.2°
+        const halfSpan = 180 - gapDeg / 2;       // ~140.4°
+
+        var options = {
+            series: [78],
+            chart: {
+                type: 'radialBar',
+                height: 180,
+                width: 220,
+                sparkline: { enabled: true }
+            },
+            plotOptions: {
+                radialBar: {
+                    startAngle: -halfSpan,             // ~ -140.4
+                    endAngle: halfSpan,                // ~ +140.4
+                    hollow: {
+                        size: '58%'                      // толще дуга
+                    },
+                    track: {
+                        background: 'rgba(242,242,242,0.85)',
+                        strokeWidth: '100%',
+                        margin: 0
+                    },
+                    dataLabels: {
+                        name: { show: false },
+                        value: {
+                            fontSize: '28px',
+                            fontWeight: 600,
+                            offsetY: 8,
+                            formatter: v => Math.round(v) + '%'
+                        }
+                    }
+                }
+            },
+            stroke: {
+                lineCap: 'butt'                      // квадратные края
+            },
+            colors: ['var(--tblr-primary)'],
+            legend: { show: false }
+        };
+
+        new ApexCharts(document.querySelector('#seo-chart'), options).render();
+
+    </script>
     @include('foxsun::pages.dashboard-footer')
 @endsection
